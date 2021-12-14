5175753938c5f9a62c6ac55610e4fa77f0a8ec97
Urbbiz <71328179+Urbbiz@users.noreply.github.com>
2021-12-14 13:32:03 +0200

    WebScraper

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ScrapySharp.Extensions;
+using ScrapySharp.Network;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -8,7 +10,28 @@ namespace WebScraper
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            /*
+            Parašyti WebScraper konsolinę programą, kuri išspausdina darbo skelbimų pavadinimus kuriuose yra toks stringas(pavyzdžiui: '.NET')
+            iš https://www.cvbankas.lt/darbo-pasiulymai-programuotojams-it-specialistams pirmojo puslapio.
+            Patarimas: Instaliuokite, panaudokite 'HTMLAgilityPack Nuget package' savo programoje.
+         */
+
+            Console.WriteLine(" We found! ");
+
+            ScrapingBrowser broswer = new ScrapingBrowser();
+
+            WebPage homePage = broswer.NavigateToPage(new Uri("https://www.cvbankas.lt/darbo-pasiulymai-programuotojams-it-specialistams"));
+
+            var html = homePage.Html;
+
+            var nodes = html.CssSelect(".list_cell h3");
+
+            var professionNames = nodes.Where(n => n.InnerText.Contains(".NET")).Select(n => n.InnerText);
+            foreach (var name in professionNames)
+            {
+                Console.WriteLine(name);
+            }
         }
     }
 }
